85908b9a1b2e0c309703b3b121c4b8391e067014
mohamedfahil <mfahil26@gmail.com>
2024-05-14 14:34:14 +0530

    update noe faahil.js

--- faahil.js
+++ faahil.js
@@ -1,12 +1,7 @@
 let x=5
 let y=2
+let z=3
 console.log("the  no. is",x);
 console.log("the no. is",y);
-let total =(x+y)
+let total =(x+y+z)
 console.log(total) 
-var name="fah"
-var name2="ina"
-var tot=(name+name2)
-console.log(name+name2)
-
-
