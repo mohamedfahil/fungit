779fea6d5bf81816edee38b789c72a3507e979da
mohamedfahil <mfahil26@gmail.com>
2024-05-14 14:15:51 +0530

    update faahil.js

--- faahil.js
+++ faahil.js
@@ -1,6 +1,8 @@
 let x=5
 let y=2
-console.log("the add of two no. is",x);
-console.log("the add of two no. is",y);
+console.log("the  no. is",x);
+console.log("the no. is",y);
+let total =(x+y)
+console.log(total)
 
 
